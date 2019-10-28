b067ef02f14b1cf5d79361200fd6da379c7d56c5
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2019-10-28 20:21:51 +0300

    Add binary search implementation

---
+++ binarySearch.js
@@ -0,0 +1,28 @@
+const { expect } = require('./assert')
+
+function binarySearch(list, number) {
+  let head = 0
+  let tail = list.length - 1
+
+  while (head <= tail) {
+    const guessedIndex = Math.floor((head + tail) / 2)
+    const guessedElement = list[guessedIndex]
+
+    if (number === guessedElement) return guessedIndex
+    if (number > guessedElement) head = guessedIndex + 1
+    else tail = guessedIndex - 1
+  }
+
+  return null
+}
+
+const LIST = [1, 2, 4, 10, 32, 45, 1002, 1250]
+
+LIST.forEach((value, index) => {
+  expect(binarySearch(LIST, value)).toBe(index)
+})
+
+expect(binarySearch(LIST, 3)).toBe(null)
+expect(binarySearch(LIST, 0)).toBe(null)
+expect(binarySearch(LIST, 1003)).toBe(null)
+expect(binarySearch(LIST, 4000)).toBe(null)
