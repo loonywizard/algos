8efc2c4c5fdbaea8371aca5aa5336b6e4e780d78
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2019-11-20 17:43:11 +0300

    Add palindrom fn

---
+++ palindrom.js
@@ -0,0 +1,25 @@
+const { expect } = require('./assert')
+
+function normalizeString(str) {
+  return str.toLowerCase().replace(/[ -.,!?]/g, '')
+}
+
+function isPalindrom(str) {
+  const normalizedStr = normalizeString(str)
+
+  let head = 0;
+  let tail = normalizedStr.length - 1
+
+  while (head < tail) {
+    if (normalizedStr[head] !== normalizedStr[tail]) return false
+
+    head++
+    tail--
+  }
+
+  return true
+}
+
+expect(isPalindrom('H3h3h')).toBe(true)
+expect(isPalindrom('Eva, can I see bees in a cave?')).toBe(true)
+expect(isPalindrom('NOTPALINDROM')).toBe(false)
