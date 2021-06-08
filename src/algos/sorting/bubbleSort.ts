bf480fb88bbe2131a2ea1bf4ae087ec731445410
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-06-09 00:46:33 +0300

    setup unit tests with jest, refactor sorting algos tests with jest

--- src/algos/sorting/bubbleSort.ts
+++ src/algos/sorting/bubbleSort.ts
@@ -1,8 +1,7 @@
-import { haveSameLength, isSorted, expect }  from '../../validators'
-import { generateRandomArray } from '../../utils/generateRandomArray'
 import { swap } from '../../utils/swap'
 
-function bubbleSort(arr: number[]): void {
+
+function bubbleSort(arr: number[]): number[] {
   const n = arr.length
 
   for (let i = 0; i < n; i++) {
@@ -12,22 +11,9 @@ function bubbleSort(arr: number[]): void {
       }
     }
   }
+
+  return arr
 }
 
-const TEST_CASES = [
-  generateRandomArray(0, 0, 0),
-  generateRandomArray(1, 1, 5),
-  generateRandomArray(1000, -1000, 1000),
-  generateRandomArray(1001, -1001, 1001),
-]
-
-for (let testCase of TEST_CASES) {
-  const copyOfTestCase = [...testCase]
-
-  bubbleSort(copyOfTestCase)
-
-  expect(isSorted(copyOfTestCase)).toBe(true)
-  expect(haveSameLength(copyOfTestCase, testCase)).toBe(true)
-}
 
 export { bubbleSort }
---
+++ src/algos/sorting/quickSort.ts
@@ -0,0 +1,31 @@
+import { swap } from '../../utils/swap'
+
+
+function partition(arr: number[], start: number, end: number): number {
+  const pivotIndex = start
+  const pivotElement = arr[pivotIndex]
+
+  while (start < end) {
+    while (start < arr.length && arr[start] <= pivotElement) start++
+    while (arr[end] > pivotElement) end--
+
+    if (start < end) swap(arr, start, end)
+  }
+
+  swap(arr, pivotIndex, end)
+
+  return end
+}
+
+
+function quickSort(arr: number[], start = 0, end = arr.length - 1) {
+  if (start >= end) return
+
+  const p = partition(arr, start, end)
+
+  quickSort(arr, start, p - 1)
+  quickSort(arr, p + 1, end)
+}
+
+
+export { quickSort }
