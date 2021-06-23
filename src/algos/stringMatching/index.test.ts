b938aedfce17afb1d66cbea1e4c4558e4f36c0ca
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-06-23 18:10:14 +0300

    add more test cases for string matching tests

--- src/algos/stringMatching/index.test.ts
+++ src/algos/stringMatching/index.test.ts
@@ -15,4 +15,22 @@ describe.each(ALGOS)('String Matching $algoName', ({ algo }) => {
 
     expect(matches).toStrictEqual([6])
   })
+
+  it('Finds no matches', () => {
+    const text = 'HELLO FROM VOVA'
+    const pattern = '__WRONG__'
+
+    const matches = algo(text, pattern)
+
+    expect(matches).toStrictEqual([])
+  })
+
+  it('Finds several matches', () => {
+    const text = '001 110 010 001 101'
+    const pattern = '001'
+
+    const matches = algo(text, pattern)
+
+    expect(matches).toStrictEqual([0, 12])
+  })
 })
