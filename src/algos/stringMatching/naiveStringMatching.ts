33109660e175a72c2b77bade437d73f5736ac156
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-06-23 18:01:49 +0300

    add naive string matching

---
+++ src/algos/stringMatching/naiveStringMatching.ts
@@ -0,0 +1,18 @@
+function naiveStringMatcher(text: string, pattern: string): number[] {
+  const n = text.length
+  const m = pattern.length
+  
+  const matches: number[] = []
+
+  for (let i = 0; i < n - m; i++) {
+    let j = 0
+
+    while (text[i + j] === pattern[j] && j < m) j++
+
+    if (j === m) matches.push(i)
+  }
+
+  return matches
+}
+
+export { naiveStringMatcher }
